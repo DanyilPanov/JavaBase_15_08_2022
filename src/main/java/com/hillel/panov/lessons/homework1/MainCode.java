84ea270fc4b4fbe8f1a88c383aa2531b2bd133a2
Danyil Panov <danjapanov@gmailcom>
2022-08-24 13:16:30 +0300

    adeed homework

---
+++ src/main/java/com/hillel/panov/lessons/homework1/MainCode.java
@@ -0,0 +1,8 @@
+package com.hillel.panov.lessons.homework1;
+
+public class MainCode {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+}
---
+++ src/main/java/com/hillel/panov/lessons/homework2/MyApp.java
@@ -0,0 +1,36 @@
+package com.hillel.panov.lessons.homework2;
+
+public class MyApp {
+    public static void main(String[] args) {
+        System.out.println("location London Bridge");
+        char charVariable = 'N';
+        char charVariable2 = 'W';
+        char charVariable3 = '°';
+        char charVariable4 = '/';
+        char charVariable5 = '\'';
+        final double VALUE = 51.50556;
+        final double VALUE2 = 0.07541;
+        final double VALUE3 = 30.333;
+        final double VALUE4 = 4.5243;
+        byte byteVariable = 51;
+        byte byteVariable2 = 0;
+        System.out.print(charVariable);
+        System.out.print(VALUE);
+        System.out.print(charVariable3);
+        System.out.print(charVariable2);
+        System.out.print(VALUE2);
+        System.out.print(charVariable3);
+        System.out.print(charVariable4);
+        System.out.print(charVariable);
+        System.out.println(byteVariable);
+        System.out.print(charVariable3);
+        System.out.print(VALUE3);
+        System.out.print(charVariable5);
+        System.out.print(charVariable2);
+        System.out.print(byteVariable2);
+        System.out.print(charVariable3);
+        System.out.print(VALUE4);
+    }
+
+
+}
